db99084ff18fbbe72ac2d03e9e5555fc0afb7d07
Kvantexis <221466872+Kvantexis@users.noreply.github.com>
2025-08-24 18:57:19 +0700

    site
    site

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+            
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+            
+            
+            // Показываем результат
+    document.getElementById('result').innerHTML = `
+        <h3>✅ Данные получены!</h3>
+        <p><strong>Имя:</strong> ${name}</p>
+        <p><strong>Телефон:</strong> ${phone}</p>
+        <p><em>В реальном проекте данные отправлялись бы на сервер</em>
+    `;
+            
+            // Очищаем форму
+    document.getElementById('myForm').reset();
+    console.log("dfdfdf")
+ });     
